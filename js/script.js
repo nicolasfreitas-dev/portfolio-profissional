9a41370f66c27d2e684c6c859fccb94ed13c0f4a
nicolasfreitas <nicolasfpdev@gmail.com>
2024-03-15 23:19:37 -0300

    implementando light e dark mode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleTheme = document.getElementById('toggle-theme')
+const main = document.querySelector('.container')
+
+toggleTheme.addEventListener("change", () => {
+    document.body.classList.toggle('light')
+})
+
