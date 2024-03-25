c49444451d1e61f42397ff8de5684502e90470d2
nicolasfreitas <nicolasfpdev@gmail.com>
2024-03-25 00:00:49 -0300

    inserindo botão para contato

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const wrapper = document.querySelector('#wrapper')
 const carousel = document.querySelector('#carousel')
 const arrowBtns = document.querySelectorAll("#wrapper i")
 const firstCardWidth = carousel.querySelector(".card").offsetWidth
-const carouselChildrens = [...carousel.children]
 
 // CHANGE THEME LIGHT/DARK MODE
 
@@ -15,16 +14,6 @@ toggleTheme.addEventListener("change", () => {
 // SLIDER PROJECT
 
 let isDragging = false, startX, startScrollLeft, timeoutId
-
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
-
-// carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-//     carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
-// })
-
-// carouselChildrens.slice(0, cardPerView).forEach(card => {
-//     carousel.insertAdjacentHTML("beforeend", card.outerHTML)
-// })
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -56,25 +45,7 @@ const autoPlay = () => {
 
 autoPlay()
 
-const infiniteScroll = () => {
-    if (carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition")
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth)
-        carousel.classList.remove("no-transition")
-
-    } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition")
-        carousel.scrollLeft = carousel.offsetWidth
-        carousel.classList.remove("no-transition")
-    }
-
-    clearTimeout(timeoutId)
-    if (!wrapper.matches(":hover")) autoPlay()
-}
-
 carousel.addEventListener("mousedown", dragStart)
 carousel.addEventListener("mousemove", dragging)
 document.addEventListener("mouseup", dragStop)
-carousel.addEventListener("scroll", infiniteScroll)
-wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId))
 wrapper.addEventListener("mouseleave", autoPlay)
